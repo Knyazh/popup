62d9c2a9d624015949374d5b8470133b129ef5a2
Hp <qnyazface@maul.ru>
2023-07-06 18:27:09 +0400

    prevent and bext image function and keydown added.

--- js/script.js
+++ js/script.js
@@ -1,32 +1,64 @@
 const smallImages = document.querySelectorAll(".images a");
 const popup = document.querySelector(".popup");
-const bigImage =document.querySelector(".popup .image-container img")
+const bigImage = document.querySelector(".popup .image-container img");
+const iconLeft = document.querySelector(".popup .image-container .icon-overlay .left-icon");
+const iconRight = document.querySelector(".popup .image-container .icon-overlay .right-icon");
+let currentImageIndex = 0;
 
-function openBigImagePopup(imgPopup){
-    imgPopup.style.display="flex";
-}
-function closeBigImagePopup(imgPopup){
-    imgPopup.style.display="none";
+function openBigImagePopup(imgPopup) {
+    imgPopup.style.display = "flex";
 }
 
-smallImages.forEach((smallImages) => {
-    smallImages.addEventListener("click", function(e) {
+function closeBigImagePopup(imgPopup) {
+    imgPopup.style.display = "none";
+}
+
+function showImage(index) {
+    const smallImageSrc = smallImages[index].getAttribute("href");
+    bigImage.setAttribute("src", smallImageSrc);
+}
+
+function showPreviousImage() {
+    currentImageIndex--;
+    if (currentImageIndex < 0) {
+        currentImageIndex = smallImages.length - 1;
+    }
+    showImage(currentImageIndex);
+}
+
+function showNextImage() {
+    currentImageIndex++;
+    if (currentImageIndex >= smallImages.length) {
+        currentImageIndex = 0;
+    }
+    showImage(currentImageIndex);
+}
+
+smallImages.forEach((smallImage, index) => {
+    smallImage.addEventListener("click", function (e) {
         e.preventDefault();
+        currentImageIndex = index;
         openBigImagePopup(popup);
-        const smallImageSrc = this.getAttribute("href");
-        bigImage.setAttribute("src", smallImageSrc);
+        showImage(currentImageIndex);
     });
-
 });
 
-document.addEventListener("click", function(e) {
-    if(e.target.classList.contains("popup")){
+document.addEventListener("click", function (e) {
+    if (e.target.classList.contains("popup")) {
         closeBigImagePopup(popup);
     }
 });
 
-document.addEventListener("keydown", function(e){
-   if(e.key==="Escape"){
-    closeBigImagePopup(popup);
-   }
+document.addEventListener("keydown", function (e) {
+    if (e.key === "Escape") {
+        closeBigImagePopup(popup);
+    } else if (e.key === "ArrowLeft") {
+        showPreviousImage();
+    } else if (e.key === "ArrowRight") {
+        showNextImage();
+    }
 });
+
+iconLeft.addEventListener("click", showPreviousImage);
+iconRight.addEventListener("click", showNextImage);
+
